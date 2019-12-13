c7bc743408c3d453901953ae136364d50bc824e2
Alexander Gluschenko <qugluschenko@gmail.com>
2019-12-13 03:28:34 +0300

    Features: Vector math, Colors, 2D drawing

--- Path-Tracer-Sharp/MainWindow.xaml.cs
+++ Path-Tracer-Sharp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,32 +17,192 @@ using System.Diagnostics;
 
 namespace PathTracerSharp
 {
+    public struct Color 
+    {
+        public static Color Black => new Color(0, 0, 0);
+        public static Color White => new Color(1, 1, 1);
+        public static Color Red => new Color(1, 0, 0);
+        public static Color Yellow => new Color(1, 1, 0);
+        public static Color Green => new Color(0, 1, 0);
+        public static Color Cyan => new Color(0, 1, 1);
+        public static Color Blue => new Color(0, 0, 1);
+
+        //
+
+        public float R, G, B;
+
+        public Color(float r, float g, float b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        public int GetRaw() 
+        {
+            return 
+                (byte)(R * 255) << 16 | 
+                (byte)(G * 255) << 8 | 
+                (byte)(B * 255);
+        }
+    }
+
+    public struct Vector 
+    {
+        public static Vector Zero => new Vector(0, 0, 0);
+        public static Vector Up => new Vector(0, 1, 0);
+        public static Vector Down => new Vector(0, -1, 0);
+        public static Vector Right => new Vector(1, 0, 0);
+        public static Vector Left => new Vector(-1, 0, 0);
+        public static Vector Forward => new Vector(0, 0, 1);
+        public static Vector Back => new Vector(0, 0, -1);
+
+        //
+
+        public float x, y, z;
+
+        public Vector(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        // public
+        public static Vector operator +(Vector a, Vector b) => new Vector(a.x + b.x, a.y + b.y, a.z + b.z);
+        public static Vector operator -(Vector a, Vector b) => new Vector(a.x - b.x, a.y - b.y, a.z - b.z);
+        public static Vector operator *(Vector a, Vector b) => new Vector(a.x * b.x, a.y * b.y, a.z * b.z);
+        public static Vector operator /(Vector a, Vector b) => new Vector(a.x / b.x, a.y / b.y, a.z / b.z);
+
+        public static Vector operator *(Vector a, float m) => new Vector(a.x * m, a.y * m, a.z * m);
+        public static Vector operator /(Vector a, float d) => new Vector(a.x / d, a.y / d, a.z / d);
+
+        public float Length => (float)Math.Sqrt(x * x + y * y + z * z);
+
+        // static
+        public static float Dot(Vector a, Vector b) => a.x * b.x + a.y * b.y + a.z * b.z;
+        public static float Distance(Vector a, Vector b) => (a - b).Length;
+
+        public static Vector Normalize(Vector a) => a / a.Length;
+    }
+
+    public struct Ray 
+    {
+        public Vector origin;
+        public Vector direction;
+
+        public Ray(Vector origin, Vector direction)
+        {
+            this.origin = origin;
+            this.direction = direction;
+        }
+    }
+
+    public class Sphere 
+    {
+        public Vector Position { get; set; }
+        public float Radius { get; set; }
+
+        public Sphere(Vector position, float radius)
+        {
+            Position = position;
+            Radius = radius;
+        }
+    }
+
+    public class Material 
+    {
+
+    }
+
     public partial class MainWindow : Window
     {
         public WriteableBitmap Bitmap { get; set; }
+
+        public int MaxDepth = 3;
+
+        readonly List<Sphere> Spheres = new List<Sphere>();
 
         public MainWindow()
         {
             InitializeComponent();
             Loaded += MainWindow_Loaded;
+            MouseMove += MainWindow_MouseMove;
+
+            Spheres.AddRange(new Sphere[] {
+                new Sphere(new Vector(-2, 0, 0), 0.5f),
+                new Sphere(new Vector(0, 0, 0), 1f),
+                new Sphere(new Vector(3, 0, 0), 1.5f),
+            });
+        }
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) 
+            {
+                var pos = e.GetPosition(Image);
+                DrawPixel(Bitmap, pos, Color.Yellow.GetRaw());
+            }
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             Bitmap = GetBitmap(Image);
 
-            var timer = Stopwatch.StartNew();
+            var w = Bitmap.PixelWidth;
+            var h = Bitmap.PixelHeight;
+            Thread thread = new Thread(() => Proc(w, h)) { IsBackground = true };
+            thread.Start();
 
-            for (int x = 0; x < 100; x++)
+            //var timer = Stopwatch.StartNew();
+
+            /*MessageBox.Show($"{timer.ElapsedMilliseconds} ms");
+            timer.Restart();*/
+        }
+
+        public void Proc(int width, int height) 
+        {
+            var backColor = new Color(.2f, .2f, .2f).GetRaw();
+            var sphereColor = Color.Green.GetRaw();
+
+            float scale = 50;
+
+            float halfX = width / 2;
+            float halfZ = height / 2;
+
+            for (int x = 0; x < width; x++)
             {
-                for (int y = 0; y < 100; y++)
-                {
-                    DrawPixel(Bitmap, new Point(x, y));
-                }
-            }
+                float posX = (x - halfX) / scale;
 
-            MessageBox.Show($"{timer.ElapsedMilliseconds} ms");
-            timer.Restart();
+                Dispatcher.Invoke(() => {
+                    for (int z = 0; z < height; z++)
+                    {
+                        float posZ = (z - halfZ) / scale;
+
+                        var pos = new Vector(posX, 0, posZ);
+                        //
+                        var color = backColor;
+
+                        foreach (var sphere in Spheres)
+                        {
+                            if (Vector.Distance(sphere.Position, pos) > sphere.Radius)
+                            {
+                                color = x ^ z;
+                            }
+                        }
+
+                        foreach (var sphere in Spheres) 
+                        {
+                            if (Vector.Distance(sphere.Position, pos) < sphere.Radius)
+                            {
+                                color = sphereColor;
+                            }
+                        }
+
+                        DrawPixel(Bitmap, new Point(x, z), color);
+                    }
+                });
+            }
         }
 
         /// <summary>
@@ -53,10 +214,7 @@ namespace PathTracerSharp
             RenderOptions.SetBitmapScalingMode(img, BitmapScalingMode.NearestNeighbor);
             RenderOptions.SetEdgeMode(img, EdgeMode.Aliased);
 
-            img.Width = Width;
-            img.Height = Height;
-
-            var bitmap = new WriteableBitmap((int)img.Width, (int)img.Height, 96, 96, PixelFormats.Bgr32, null);
+            var bitmap = new WriteableBitmap((int)ActualWidth, (int)ActualHeight, 96, 96, PixelFormats.Bgr32, null);
 
             img.Source = bitmap;
             img.Stretch = Stretch.None;
@@ -64,43 +222,89 @@ namespace PathTracerSharp
             return bitmap;
         }
 
-        void DrawPixel(WriteableBitmap bitmap, Point point)
+        void DrawPixel(WriteableBitmap bitmap, Point point, int color)
         {
-            int column = (int)point.X;
-            int row = (int)point.Y;
+            int x = (int)point.X;
+            int y = (int)point.Y;
+
+            if (!(x >= 0 && y >= 0 && x < bitmap.PixelWidth && y < bitmap.PixelHeight)) return;
 
             try
             {
-                // Reserve the back buffer for updates.
+                // Reserve the back buffer for updates
                 bitmap.Lock();
 
                 unsafe
                 {
-                    // Get a pointer to the back buffer.
+                    // Get a pointer to the back buffer
                     IntPtr pBackBuffer = bitmap.BackBuffer;
+                    
+                    // Find the address of the pixel to draw
+                    pBackBuffer += y * bitmap.BackBufferStride;
+                    pBackBuffer += x * 4;
 
-                    // Find the address of the pixel to draw.
-                    pBackBuffer += row * bitmap.BackBufferStride;
-                    pBackBuffer += column * 4;
-
-                    // Compute the pixel's color.
-                    int color_data = 
-                        255 << 16 | // R
-                        128 << 8 |  // G
-                        255 << 0;   // B
-
-                    // Assign the color data to the pixel.
-                    *(int*)pBackBuffer = color_data;
+                    // Assign the color data to the pixel
+                    *(int*)pBackBuffer = color;
                 }
 
-                // Specify the area of the bitmap that changed.
-                bitmap.AddDirtyRect(new Int32Rect(column, row, 1, 1));
+                // Specify the area of the bitmap that changed
+                bitmap.AddDirtyRect(new Int32Rect(x, y, 1, 1));
             }
             finally
             {
-                // Release the back buffer and make it available for display.
+                // Release the back buffer and make it available for display
                 bitmap.Unlock();
             }
         }
+
+        /*Color TracePath(Ray ray, int depth)
+        {
+            if (depth >= MaxDepth)
+            {
+                return Color.Black;  // Bounced enough times
+            }
+
+            ray.FindNearestObject();
+            if (ray.hitSomething == false)
+            {
+                return Color.Black;  // Nothing was hit
+            }
+
+            Material material = ray.thingHit->material;
+            Color emittance = material.emittance;
+
+            // Pick a random direction from here and keep going
+            Ray newRay;
+            newRay.origin = ray.pointWhereObjWasHit;
+
+            // This is NOT a cosine-weighted distribution!
+            newRay.direction = RandomUnitVectorInHemisphereOf(ray.normalWhereObjWasHit);
+
+            // Probability of the newRay
+            const double p = 1.0 / (2.0 * Math.PI);
+
+            // Compute the BRDF for this ray (assuming Lambertian reflection)
+            float cos_theta = DotProduct(newRay.direction, ray.normalWhereObjWasHit);
+            Color BRDF = material.reflectance / Math.PI;
+
+            // Recursively trace reflected light sources.
+            Color incoming = TracePath(newRay, depth + 1);
+
+            // Apply the Rendering Equation here.
+            return emittance + (BRDF * incoming * cos_theta / p);
+        }
+
+        void Render(Image finalImage, count numSamples)
+        {
+            foreach (pixel in finalImage)
+            {
+                foreach (i in numSamples)
+                {
+                    Ray r = camera.generateRay(pixel);
+                    pixel.color += TracePath(r, 0);
+                }
+                pixel.color /= numSamples;  // Average samples.
+            }
+        }*/
     }
 }
