a8a1f6eeb8cdbc5aa5477a44c463b0682f7d4d35
Alexander Gluschenko <qugluschenko@gmail.com>
2020-03-03 00:21:59 +0300

    [feat] Add PerlinNoise

--- Path-Tracer-Sharp/Core/MathHelpres.cs
+++ Path-Tracer-Sharp/Core/MathHelpres.cs
@@ -7,22 +7,45 @@ namespace PathTracerSharp.Core
     {
         const double deg2rad = Math.PI / 180.0;
         const double rad2deg = 180.0 / Math.PI;
-
+        /**/
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double DegToRad(double deg) => deg * deg2rad;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double RadToDeg(double rad) => rad * rad2deg;
-
+        /**/
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double Clamp(double val, double low, double high) => Math.Max(low, Math.Min(high, val));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double Clamp(double val) => Math.Max(0, Math.Min(1, val));
+        /**/
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Clamp(int val, int low, int high) => Math.Max(low, Math.Min(high, val));
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Clamp(int val) => Math.Max(0, Math.Min(1, val));
+        /**/
+
+        /// <summary>
+        /// Линейная интерполяция
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double Lerp(double a, double b, double r) => a + (b - a) * r;
 
+        /// <summary>
+        /// Линейная интерполяция
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Lerp(int a, int b, double r) => (int)(a + (b - a) * r);
+
+        /// <summary>
+        /// Линейная интерполяция
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long Lerp(long a, long b, double r) => (long)(a + (b - a) * r);
+
+        /**/
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool SolveQuadratic(ref double a, ref double b, ref double c, ref double x0, ref double x1) 
         { 
---
+++ Path-Tracer-Sharp/Core/Rand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PathTracerSharp.Core
+{
+    public static class Rand
+    {
+        private static Random random = new Random(RandomizeSeed());
+
+        public static void InitState(int seed)
+        {
+            random = null;
+            random = new Random(seed);
+        }
+
+        public static int Int(int min, int max) => random.Next(min, max);
+        public static int Int() => Int(int.MinValue, int.MaxValue);
+
+        public static double Double(double min, double max) => MathHelpres.Lerp(min, max, Double());
+        public static double Double() => random.NextDouble();
+
+        public static long Long(long min, long max) => MathHelpres.Lerp(min, max, Double());
+        public static long Long() => Long(long.MinValue, long.MaxValue);
+
+        /// <summary>
+        /// Генерация начального состояния (seed) со значительно сниженной предсказуемостью
+        /// </summary>
+        public static int RandomizeSeed() => Guid.NewGuid().GetHashCode();
+        /// <summary>
+        /// Сброс состояния
+        /// </summary>
+        public static void Reset() => random = new Random(RandomizeSeed());
+    }
+}
+
