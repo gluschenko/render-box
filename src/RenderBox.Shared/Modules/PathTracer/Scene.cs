96e78019366238cd17c241abcf22e170896030c5
Alexander Gluschenko <qugluschenko@gmail.com>
2021-07-18 14:41:28 +0300

    Update Scene.cs

--- src/RenderBox.Shared/Modules/PathTracer/Scene.cs
+++ src/RenderBox.Shared/Modules/PathTracer/Scene.cs
@@ -6,30 +6,20 @@ namespace RenderBox.Shared.Modules.PathTracer
 {
     public class Scene
     {
-        public Color BackgroundColor { get; set; }
-        public Color AmbientColor { get; set; }
-        public List<Shape> Shapes { get; set; }
-        public IEnumerable<Light> Lights { get; set; }
+        public Color BackgroundColor { get; set; } = new Color(.2f, .2f, .2f);
+        public Color AmbientColor { get; set; } = new Color(.1f, .1f, .1f);
+        public List<Shape> Shapes { get; set; } = new List<Shape>();
+        public IEnumerable<Light> Lights { get; private set; }
 
-        public bool LightingEnabled { get; set; }
-        public bool ShadowsEnabled { get; set; }
-        public bool SoftShadows { get; set; }
-        public bool AmbientOcclusion { get; set; }
+        public bool LightingEnabled { get; set; } = true;
+        public bool ShadowsEnabled { get; set; } = true;
+        public bool SoftShadows { get; set; } = true;
+        public bool AmbientOcclusion { get; set; } = true;
 
-        public int GISamples { get; set; }
+        public int GISamples { get; set; } = 16;
 
         public Scene()
         {
-            BackgroundColor = new Color(.2f, .2f, .2f);
-            AmbientColor = new Color(.1f, .1f, .1f);
-            Shapes = new List<Shape>();
-
-            LightingEnabled = true;
-            ShadowsEnabled = true;
-            SoftShadows = true;
-            AmbientOcclusion = true;
-
-            GISamples = 32;
         }
 
         public void UpdateLights()
